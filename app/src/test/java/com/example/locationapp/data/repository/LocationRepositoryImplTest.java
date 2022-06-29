5745de96466c2989b1c348f32c564fb8eb0de466
lucas <lucas.ngo@geocomply.com>
2022-06-29 13:00:06 +0700

    Update master

--- app/src/test/java/com/example/locationapp/data/repository/LocationRepositoryImplTest.java
+++ app/src/test/java/com/example/locationapp/data/repository/LocationRepositoryImplTest.java
@@ -1,87 +1,10 @@
 package com.example.locationapp.data.repository;
 
-<<<<<<< HEAD
-import com.example.locationapp.data.sources.remote.LocationAPI;
-import com.example.locationapp.domain.repository.LocationRepository;
-
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
-
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-
-import okhttp3.OkHttpClient;
-import okhttp3.mockwebserver.MockResponse;
-import okhttp3.mockwebserver.MockWebServer;
-import okio.Buffer;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
-
-@RunWith(MockitoJUnitRunner.class)
-public class LocationRepositoryImplTest {
-    private String testDocumentPath = "/assets/success_response.json";
-    private MockWebServer mockWebServer;
-    private OkHttpClient client;
-
-    private LocationAPI api;
-
-    private LocationRepository locationRepositoryImpl;
-
-//    @Before
-//    public void setUp() {
-//        mockWebServer = new MockWebServer();
-//
-//        client = new OkHttpClient.Builder().connectTimeout(1, TimeUnit.SECONDS)
-//                .readTimeout(1, TimeUnit.SECONDS)
-//                .writeTimeout(1, TimeUnit.SECONDS)
-//                .build();
-//
-//        api = new Retrofit.Builder().baseUrl(mockWebServer.url("/"))
-//                .client(client)
-//                .addConverterFactory(GsonConverterFactory.create())
-//                .build()
-//                .create(LocationAPI.class);
-//
-//        locationRepositoryImpl = new LocationRepositoryImpl(api);
-//
-//
-//        try {
-//            mockWebServer.start(8080);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    @After
-//    public void tearDown() {
-//        try {
-//            mockWebServer.shutdown();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
-    @Test
-    public void test() {
-        String reader = new MockResponseFileReader().content;
-        Assert.assertNotNull(reader);
-    }
-
-=======
 import androidx.annotation.NonNull;
 import androidx.arch.core.executor.ArchTaskExecutor;
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.Observer;
 
 import com.example.locationapp.data.sources.local.LocalRepository;
 import com.example.locationapp.data.sources.local.LocalRepositoryImpl;
@@ -181,5 +104,4 @@ public class LocationRepositoryImplTest {
         Assert.assertEquals(result.getValue().getData(), locationDetail);
         System.out.println(result.getValue().getData());
     }
->>>>>>> Final
 }
--- app/src/test/java/com/example/locationapp/presentation/locationlist/viewmodel/LocationListViewModelTest.java
+++ app/src/test/java/com/example/locationapp/presentation/locationlist/viewmodel/LocationListViewModelTest.java
@@ -3,21 +3,10 @@ package com.example.locationapp.presentation.locationlist.viewmodel;
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 import androidx.lifecycle.MutableLiveData;
 
-<<<<<<< HEAD
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNull;
-
-import com.example.locationapp.DaggerTestLocationComponent;
-import com.example.locationapp.TestLocationComponent;
-import com.example.locationapp.TestLocationModule;
-import com.example.locationapp.data.sources.remote.model.Root;
-=======
 import com.example.locationapp.data.repository.LocationRepository;
 import com.example.locationapp.data.repository.LocationRepositoryImpl;
 import com.example.locationapp.data.sources.model.preferlocation.Location;
 import com.example.locationapp.utils.Resource;
->>>>>>> Final
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -25,14 +14,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-<<<<<<< HEAD
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
-@RunWith(MockitoJUnitRunner.class)
-public class LocationListViewModelTest {
-=======
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,39 +23,11 @@ public class LocationListViewModelTest {
     public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
 
     LocationRepository repository = Mockito.mock(LocationRepositoryImpl.class);
->>>>>>> Final
 
     LocationListViewModel viewModel;
 
     @Before
     public void setUp() {
-<<<<<<< HEAD
-        TestLocationComponent testLocationComponent = DaggerTestLocationComponent.builder().locationModule(new TestLocationModule())
-                .build();
-        viewModel = new LocationListViewModel();
-        testLocationComponent.inject(viewModel);
-    }
-
-    @Test
-    public void doSomething() {
-        assertNull(viewModel.getLoading().getValue());
-        viewModel.getPreferLocationsUseCase.execute().enqueue(new Callback<Root>() {
-            @Override
-            public void onResponse(Call<Root> call, Response<Root> response) {
-                assertEquals(viewModel.getLoading().getValue(), false);
-            }
-
-            @Override
-            public void onFailure(Call<Root> call, Throwable t) {
-                assertNotEquals(viewModel.getError_msg().getValue(), null);
-            }
-        });
-
-    }
-    @Test
-    public void doIt() {
-        assertEquals(1+1,2);
-=======
         viewModel = Mockito.spy(new LocationListViewModel(repository));
     }
 
@@ -110,6 +63,5 @@ public class LocationListViewModelTest {
 
         Assert.assertFalse(viewModel.getLoading().getValue());
         Assert.assertEquals(viewModel.getError_message().getValue(), response.getError());
->>>>>>> Final
     }
 }
